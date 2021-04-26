d91cc7804c1894e3c9d334088a8812683dc648dd
MaclaurinSeries <63801107+MaclaurinSeries@users.noreply.github.com>
2021-04-26 23:16:28 +0700

    dummy

---
+++ generateDummy/dummy/main.cpp
@@ -0,0 +1,12 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+
+}
