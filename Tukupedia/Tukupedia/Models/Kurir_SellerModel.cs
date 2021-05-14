af78b128de7b4837dcb6c9ad2e17ba1722312d5f
tofutofu2 <63796916+tofutofu2@users.noreply.github.com>
2021-05-14 22:21:50 +0700

    Seller Page Info Toko done

---
+++ Tukupedia/Tukupedia/Models/Kurir_SellerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tukupedia.Models {
+    public class Kurir_SellerModel : Model {
+        public Kurir_SellerModel() {
+            TableName = "KURIR_SELLER";
+            init();
+        }
+    }
+}
