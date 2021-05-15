548a4803d22b55c9af12a28f38f7b83668aa16ff
Christian Chen <christianchen01@gmail.com>
2021-05-15 23:00:56 +0700

    init shop cart component

--- Tukupedia/Tukupedia/Components/ShopCartComponent.cs
+++ Tukupedia/Tukupedia/Components/ShopCartComponent.cs
@@ -6,7 +6,13 @@ namespace Tukupedia.Components
     public class ShopCartComponent:Card
     {
         private StackPanel spMain;
+        private StackPanel spDesc;
+        private StackPanel spCart;
+        private StackPanel spContent;
+        private StackPanel spSubtotal;
+        private StackPanel spKurir;
         private TextBlock namaToko;
+        private TextBlock isOfficial;
         private TextBlock pilihDurasi;
         private TextBlock subTotal;
         private TextBlock labelsubTotal;
@@ -15,6 +21,37 @@ namespace Tukupedia.Components
 
         public ShopCartComponent()
         {
+            spMain = new StackPanel();
+            spDesc = new StackPanel();
+            spContent = new StackPanel();
+            spCart = new StackPanel();
+            spSubtotal = new StackPanel();
+            spKurir = new StackPanel();
+            namaToko = new TextBlock();
+            isOfficial = new TextBlock();
+            pilihDurasi = new TextBlock();
+            subTotal = new TextBlock();
+            labelsubTotal = new TextBlock();
+            cbKurir = new ComboBox();
+
+            spDesc.Children.Add(namaToko);
+            //Check if Official
+            spDesc.Children.Add(isOfficial);
+
+            spKurir.Children.Add(pilihDurasi);
+            spKurir.Children.Add(cbKurir);
+            
+            spContent.Orientation = Orientation.Horizontal;
+            spContent.Children.Add(spCart);
+            spContent.Children.Add(spKurir);
+
+            spSubtotal.Children.Add(labelsubTotal);
+            spSubtotal.Children.Add(subTotal);
+            
+            spMain.Children.Add(spDesc);
+            spMain.Children.Add(spContent);
+            spMain.Children.Add(spSubtotal);
+
         }
     }
 }
