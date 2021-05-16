2247604251fc4b2f8ab2ec7b5d1378b6a2c5a852
Christian Chen <christianchen01@gmail.com>
2021-05-16 20:30:23 +0700

    ADd Stored Prochedure

---
+++ Tukupedia/Tukupedia/Helpers/DatabaseHelpers/StoredProchedure.cs
@@ -0,0 +1,74 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tukupedia.Helpers.DatabaseHelpers
+{
+    public class StoredProchedure
+    {
+        public string prochedure_name { get; set; }
+        public OracleCommand cmd { get; set; }
+        Dictionary<string, ParameterDirection> directions;
+        //Contoh Cara Pakai
+        //OracleStoredProchedure autogen = new OracleStoredProchedure("autogenNota");
+        //autogen.addParam("I", "tgl",tgl,255,OracleDbType.Varchar2);
+        //autogen.addParam("R", "return",255, OracleDbType.Varchar2);
+        //tbNomorNota.Text = autogen.getValue();
+
+        public StoredProchedure(string prochedure_name)
+        {
+            this.prochedure_name = prochedure_name;
+            directions = new Dictionary<string, ParameterDirection>();
+            directions.Add("I", ParameterDirection.Input);
+            directions.Add("IO", ParameterDirection.InputOutput);
+            directions.Add("R", ParameterDirection.ReturnValue);
+            directions.Add("O", ParameterDirection.Output);
+            initProchedure();
+        }
+
+        public void initProchedure()
+        {
+            cmd = new OracleCommand()
+            {
+                Connection = App.connection,
+                CommandText = prochedure_name,
+                CommandType = CommandType.StoredProcedure
+            };
+        }
+        public void addParam(string direction, string paramName, int size, OracleDbType type)
+        {
+            cmd.Parameters.Add(new OracleParameter()
+            {
+                Direction = directions[direction],
+                ParameterName = paramName,
+                OracleDbType = type,
+                Size = size
+            });
+        }
+        public void addParam(string direction, string paramName, string input, int size, OracleDbType type)
+        {
+            cmd.Parameters.Add(new OracleParameter()
+            {
+                Direction = directions[direction],
+                ParameterName = paramName,
+                OracleDbType = type,
+                Size = size,
+                Value = input,
+            });
+
+        }
+        public string getValue(string ret = "ret")
+        {
+            App.openConnection(out _);
+            cmd.ExecuteNonQuery();
+            string value = cmd.Parameters[ret].Value.ToString();
+            App.closeConnection(out _);
+
+            return value;
+        }
+    }
+}
