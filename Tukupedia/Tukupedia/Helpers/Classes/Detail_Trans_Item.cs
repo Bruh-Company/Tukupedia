67758870676a3e6b5960c55a4d1b112fbeccce11
Christian Chen <christianchen01@gmail.com>
2021-05-19 23:17:43 +0700

    Update View on Transaction

---
+++ Tukupedia/Tukupedia/Helpers/Classes/Detail_Trans_Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tukupedia.Helpers.Classes
+{
+    public class Detail_Trans_Item
+    {
+    }
+}
---
+++ Tukupedia/Tukupedia/ViewModels/Customer/TransactionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Tukupedia.ViewModels.Customer
+{
+    public class TransactionViewModel
+    {
+        public static void initH_Trans(DataGrid grid)
+        {
+            
+        }
+    }
+}
