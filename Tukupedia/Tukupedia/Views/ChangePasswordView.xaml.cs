5ecb7d1518f791dded63fff7ac2db4743d861596
marmar <andreasiskandar43@gmail.com>
2021-05-20 20:55:56 +0700

    push conflict

--- Tukupedia/Tukupedia/Views/ChangePasswordView.xaml.cs
+++ Tukupedia/Tukupedia/Views/ChangePasswordView.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,23 +8,23 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Tukupedia.Helpers.Utils;
-using Tukupedia.ViewModels.Customer;
+using Tukupedia.Models;
 
 namespace Tukupedia.Views
 {
     /// <summary>
-    /// Interaction logic for ItemDetailView.xaml
+    /// Interaction logic for ChangePasswordView.xaml
     /// </summary>
-    public partial class ItemDetailView : Window
+    public partial class ChangePasswordView : Window
     {
-        public ItemDetailView()
+        PasswordModel pm;
+        public ChangePasswordView(string tabel, string id)
         {
-
+            pm = new PasswordModel(tabel, id);
+            InitializeComponent();
         }
     }
 }
